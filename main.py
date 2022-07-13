ab5f449b0461ce6086197730653674ffda9df91d
Olof Beckman <olof.beckman@gmail.com>
2022-07-13 18:31:55 +0200

    Sort of works now. Now for some more evil tests

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from rich import print
 import click
+from copy import copy
 
 @dataclass
 class Bid:
@@ -13,14 +14,43 @@ class Auction:
     def __init__(self):
         self.bids = []
 
+    def sanity_check(self):
+        pass
+
+    def last_bid_highest_max(self) -> bool:
+        return False
+
+    def last_bid_higher_than_all_max(self) -> bool:
+        return False
+
     def new_bid(self, bid: Bid):
         if leader := self.leader:
-            if bid.amount < leader.amount:
-                print(f"Illegal bid! Amount is lower than the current leader: {leader}")
-                return
+            if bid.amount <= leader.amount:
+                raise(ValueError(f"Illegal bid! Amount is lower than the current leader: {leader}"))
             self.bids.append(bid)
+            
+            higher_bids = sorted(filter(lambda x: x.max_amount > bid.amount, self.bids), key=lambda x: x.max_amount)
+            
+            for bid_to_raise in higher_bids[:-1]:
+                auto_bid = copy(bid_to_raise)
+                auto_bid.amount = auto_bid.max_amount
+                self.bids.append(auto_bid)
+            
+            new_leader = higher_bids[-1]
+            if self.bids[-1] != new_leader:
+                auto_bid = copy(new_leader)
+                # auto_bid.amount = self.bids[-1].amount + 1
+                auto_bid.amount = min(self.bids[-1].amount + 1, auto_bid.max_amount)
+                self.bids.append(auto_bid)
         else:
             self.bids.append(bid)
+
+    def get_highest_max(self):
+        """Returns the bids with the highest max_amount"""
+        sorted_bids = sorted(self.bids, key=lambda x: x.max_amount)
+        highest_max = sorted_bids[-1]
+        return highest_max
+        # return list(filter(lambda x: x.max_amount >= highest_max, sorted_bids))
 
     @property
     def leader(self):
@@ -44,14 +74,19 @@ def main():
     auction = Auction()
 
     while True:
-        print(auction)
+        print(auction.bids)
+        print("_______")
         user = click.prompt("Who will make the next bid?", type=int) - 1
         if user >= 0 and user < user_count:
             amount = click.prompt("How much are they bidding?", type=int)
             max_amount = click.prompt("How far are they willing to go?", default=amount)
 
             bid = Bid(user, amount, max_amount)
-            auction.new_bid(bid)
+
+            try:
+                auction.new_bid(bid)
+            except ValueError as e:
+                print(e)
 
 
 if __name__=='__main__':
--- test_main.py
+++ test_main.py
@@ -1,9 +1,56 @@
 from pytest import raises
 from main import Auction, Bid
 
+def test_bid_equality():
+    assert Bid(1,10,10) == Bid(1,10,10)
+
 def test_equal_bid_raises():
     auction = Auction()
     auction.new_bid(Bid(1,10,10))
 
     with raises(ValueError):
         auction.new_bid(Bid(1,10,10))
+
+def test_right_highest_max():
+    auction = Auction()
+    auction.bids = [
+        Bid(1,10,20),
+        Bid(1,10,10),
+        Bid(1,10,30),
+        Bid(1,10,15),
+    ]
+
+    assert auction.get_highest_max() == Bid(1,10,30)
+
+def test_auto_raise_first_bidder_should_win():
+    auction = Auction()
+    auction.new_bid(Bid(1,10,20))
+
+    auction.new_bid(Bid(2,11,15))
+
+    assert auction.leader == Bid(1,16,20)
+
+
+def test_auto_raise_seconde_bidder_should_win():
+    auction = Auction()
+    auction.new_bid(Bid(1,16,20))
+
+    auction.new_bid(Bid(2,17,23))
+
+    print(auction.bids)
+    assert auction.leader == Bid(2,21,23)
+
+
+def test_auto_raise_should_never_result_in_exceeded_max():
+    auction = Auction()
+
+    bids = [
+        Bid(user=2, amount=21, max_amount=25),
+        Bid(user=1, amount=22, max_amount=25),
+    ]
+
+    for bid in bids:
+        auction.new_bid(bid)
+
+    for bid in auction.bids:
+        assert bid.amount <= bid.max_amount
